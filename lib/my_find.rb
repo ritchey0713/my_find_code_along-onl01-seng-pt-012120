22125deec582e4b361162bc4ee3f88137838a4bf
David Ritchey <ritchey0713@gmail.com>
2020-01-30 23:34:09 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
-def my_find(collection)
 
-end
+def my_find(collection)
+  element = 0
+  while element < collection.length
+    return collection[element] if yield(collection[element])
+    element += 1
+  end 
+  # return nil 
+end 
